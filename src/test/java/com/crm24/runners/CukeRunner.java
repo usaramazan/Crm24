a92058722714c2e1e55a786343eb99df247d0099
Ramazan Aksit <usaramazan@gmail.com>
2020-08-04 12:36:26 -0400

    updated report

--- src/test/java/com/crm24/runners/CukeRunner.java
+++ src/test/java/com/crm24/runners/CukeRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/crm24/stepdefinitions",
         dryRun = false,
-        tags = "@smoke"
+        tags = "@smoke_test"
 )
 
 public class CukeRunner {
