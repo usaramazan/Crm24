e449ccc17bb02379fe8104efb4d8c390352fad42
Ramazan Aksit <usaramazan@gmail.com>
2020-08-04 10:31:17 -0400

    updated report

--- src/test/java/com/crm24/runners/CukeRunner.java
+++ src/test/java/com/crm24/runners/CukeRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"html:target/cucumber-report.html","json:target/cucumber.json","rerun:target/rerun.txt","junit:target/cucumber.xml"},
-        //"json:target/cucumber.jason","rerun:target/rerun.txt"
+        //"json:target/cucumber.json","rerun:target/rerun.txt"
         features = "src/test/resources/features",
         glue = "com/crm24/stepdefinitions",
         dryRun = false,
